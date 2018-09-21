a8721014907619385176d6a6e0d8a4ef2d01a414
“Nick <amellocello@msn.com>
2018-09-21 16:51:35 -0700

    Create example front page.

--- src/components/ConfirmCheckout.jsx
+++ src/components/ConfirmCheckout.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 function ConfirmCheckout(){
   return (
     <div>
-       <h2>Order Confirmation</h2>
-       <div>Thank you for your order!</div>
-       <div><Link to="/">Back to Home</Link></div>
-       <div><Link to="/browse">Back to Browse</Link></div>
-       <div><Link to="/order-history">View Order History</Link></div>
+      <h2>Order Confirmation</h2>
+      <div>Thank you for your order!</div>
+      <div><Link to="/">Back to Home</Link></div>
+      <div><Link to="/browse">Back to Browse</Link></div>
+      <div><Link to="/order-history">View Order History</Link></div>
     </div>
   );
 }
